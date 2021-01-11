0e00cfb117818c0429deffaca66dbf844ae1b39a
Hemant <prasadsai.ahk@gmail.com>
2021-01-11 22:26:49 +0530

    main

--- app/main.py
+++ app/main.py
@@ -89,6 +89,6 @@ def signin():
     return redirect(url, code=303)
 
 
-if __name__ == "__main__":
-    # Only for debugging while developing
-    api.run(host='0.0.0.0', debug=True, port=80)
+# if __name__ == "__main__":
+#     # Only for debugging while developing
+#     api.run(host='0.0.0.0', debug=True, port=80)
